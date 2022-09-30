38e5c02a35eeef33be0d34bee5b7a45b1ff5a1dd
vhavhugo <vhavhugo@hotmail.com>
2022-09-29 22:18:41 -0300

    Colocando html

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return "<h1>Hello World</h1>"
+    return render_template("index.html")
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
