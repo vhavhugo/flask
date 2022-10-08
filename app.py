27f50411e5125cfa1f8a03e06eaf4cc1966a1342
vhavhugo <vhavhugo@hotmail.com>
2022-10-08 17:12:24 -0300

    Variaveis

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return render_template("index.html")
+    fruta1 = "Morango"
+    fruta2 = "Uva"
+    fruta3 = "Maça"
+    fruta4 = "Laranja"
+    return render_template("index.html", fruta1=fruta1,
+                                        fruta2=fruta2,
+                                        fruta3=fruta3,
+                                        fruta4=fruta4)
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+#http://127.0.0.1:5000
