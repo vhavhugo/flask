d639982700aba42c8d52521ad9e924b03a8ab8bd
vhavhugo <vhavhugo@hotmail.com>
2022-10-08 20:23:32 -0300

    Adicionando dados a um dicionario

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+#from crypt import methods
 from flask import Flask, render_template, request
 app = Flask(__name__)
 frutas = []
+registros = []
 
 @app.route('/', methods=["GET", "POST"])
 def principal():
@@ -11,9 +13,13 @@ def principal():
             frutas.append(request.form.get("fruta"))
     return render_template("index.html", frutas=frutas)
 
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno":8.0}
-    return render_template('sobre.html', notas=notas)
+    # notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno":8.0}
+    if request.method == "POST":
+        if request.form.get("aluno") and request.form.get("nota"):
+            registros.append({"aluno": request.form.get("aluno"), "nota": request.form.get("nota")})
+    
+    return render_template('sobre.html', registros=registros)
 
 #http://127.0.0.1:5000
